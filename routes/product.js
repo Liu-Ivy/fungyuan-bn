c6b4fd19e291bbd585d8a0be8e2a5777ce0db3d2
Ivy Liu <ivyliu402@gmail.com>
2019-07-22 20:38:03 +0200

    addproduct done

--- routes/product.js
+++ routes/product.js
@@ -4,6 +4,7 @@ const createError = require('http-errors');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const Product = require('../models/product');
+const parser = require('../config/cloudinary');
 
 //GET '/product'
 router.get('/:id', (req, res) => {
@@ -19,18 +20,33 @@ router.get('/:id', (req, res) => {
     })
 });
 
-// //POST '/product'
-// router.post('/', ( req, res, next) =>{
-//   const { name, img, description, category_id} = req.body;
+//POST '/product'
+router.post('/', ( req, res, next) =>{
+  const { name, imageUrl, description, category_id} = req.body;
+  const newProduct = new Product({name, img: imageUrl, description, category_id})
   
-//   Product.create({ name, img, description, category_id})
-//   .than((response)=>{
-//     res.status(201).jason(response);
-//   })
-//   .catch((err)=>{
-//     res.status(500).json(err);
-//   })
-// })
+  // console.log('req.body', name,imageUrl, description, category_id)
+  // const saveProduct = newProduct.save()
+  
+  Product.create({ name, img: imageUrl, description, category_id})
+  .then((response)=>{
+    res.status(201).json(response);
+  })
+  .catch((err)=>{
+    res.status(500).json(err);
+  })
+})
+
+//POST '/image'
+router.post('/image', parser.single('photo'), (req, res, next) => {
+  console.log('file upload');
+  if (!req.file) {
+    next(new Error('No file uploaded!'));
+  };
+  const imageUrl = req.file.secure_url;
+  res.json(imageUrl).status(200);
+});
+
 
 
 module.exports = router;
