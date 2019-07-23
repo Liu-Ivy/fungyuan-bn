89ee61e3e71eb873179c37bf2f82777935805bdf
Ivy Liu <ivyliu402@gmail.com>
2019-07-23 21:24:39 +0200

    order List

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,6 @@ const {
 } = require('../helpers/middlewares');
 
 router.get('/me', isLoggedIn(), (req, res, next) => {
-  console.log('req.session.currentUser', req.session.currentUser)
   res.json(req.session.currentUser);
 });
 
