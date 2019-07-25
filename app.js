517479af4a4b8412a57aebb1748e50e8024c23b6
Ivy Liu <ivyliu402@gmail.com>
2019-07-25 16:59:50 +0200

    Add public folder

--- app.js
+++ app.js
@@ -70,6 +70,12 @@ app.use('/category', category);
 app.use('/product',product);
 app.use('/orderList',orderList);
 
+// REACT APP index.html	
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   res.status(404).json({ code: 'not found' });
